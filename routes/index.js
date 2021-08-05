839d235e6c11b09f6d778c56e1ca86ca78d754b6
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-08-05 18:48:42 +0100

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.post('/new', (req, res, next) => {
+  const messageText = req.body.messageText
+  const username = req.body.username
+  messages.push({ text: messageText, user: username, added: new Date() })
+
+  res.redirect('/')
+})
+
+module.exports = router;
